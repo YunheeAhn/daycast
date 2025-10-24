901cad7d54ffc918aa06ab225f29e6774c5890ae
YunheeAhn <ayh95944674@gmail.com>
2025-10-25 00:02:13 +0900

    style : 클래스명 추가

--- src/component/WeatherButtons.jsx
+++ src/component/WeatherButtons.jsx
@@ -3,14 +3,18 @@ import React from "react";
 const WeatherButtons = ({ cities, setCity }) => {
   return (
     <div>
-      <button onClick={() => setCity("current")}>현재 위치</button>
+      <button onClick={() => setCity("current")} className="current-city">
+        현재 위치
+      </button>
       {/* map 함수로 불러오기 */}
 
-      {cities.map((city) => (
-        <button key={city} onClick={() => setCity(city)}>
-          {city}
-        </button>
-      ))}
+      <div className="button-wrap">
+        {cities.map((city) => (
+          <button key={city} onClick={() => setCity(city)}>
+            {city}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
