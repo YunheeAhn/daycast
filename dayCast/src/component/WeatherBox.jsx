9f8b1e46fd19534c5915f383d7bd17277619e44a
YunheeAhn <ayh95944674@gmail.com>
2025-10-24 15:57:54 +0900

    feat : api 활용하여 현재위치 날씨 불러오기

---
+++ dayCast/src/component/WeatherBox.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const WeatherBox = ({ weather }) => {
+  if (!weather) {
+    console.log("날씨 정보 없어!!!");
+    return null; // 날씨 정보가 없으면 아무것도 렌더링하지 않음
+  }
+  const fah = Math.floor((weather.main.temp * 9) / 5 + 32);
+  const cel = Math.floor(weather.main.temp);
+
+  console.log("whether??", weather);
+  return (
+    <div className="weather-box">
+      <div>{weather?.name}</div>
+      <div>
+        섭씨 {weather ? cel : "로딩중..."}도 / 화씨 {weather ? fah : "로딩중..."}도
+      </div>
+      <div>{weather?.weather[0].description}</div>
+    </div>
+  );
+};
+
+export default WeatherBox;
