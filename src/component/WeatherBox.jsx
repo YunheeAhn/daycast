a8f03076e12285601766970f54f2f5df0d6a341e
YunheeAhn <ayh95944674@gmail.com>
2025-10-24 22:26:36 +0900

    feat : 날씨 아이콘 불러오기 추가

--- src/component/WeatherBox.jsx
+++ src/component/WeatherBox.jsx
@@ -7,11 +7,14 @@ const WeatherBox = ({ weather }) => {
   }
   const fah = Math.floor((weather.main.temp * 9) / 5 + 32);
   const cel = Math.floor(weather.main.temp);
+  const iconCode = weather.weather[0].icon;
+  const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 
   console.log("whether??", weather);
   return (
     <div className="weather-box">
       <div>{weather?.name}</div>
+      <img src={iconUrl} alt="weather-img"></img>
       <div>
         섭씨 {weather ? cel : "로딩중..."}도 / 화씨 {weather ? fah : "로딩중..."}도
       </div>
