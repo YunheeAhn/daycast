97c830c8076e17b322cfd24ce3976a83a9ac0d5b
YunheeAhn <ayh95944674@gmail.com>
2025-10-25 21:45:51 +0900

    style : 버튼 활성화시 active 클래스 추가

--- src/component/WeatherButtons.jsx
+++ src/component/WeatherButtons.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 
-const WeatherButtons = ({ cities, setCity }) => {
+const WeatherButtons = ({ cities, setCity, selectedCity }) => {
   return (
     <div>
-      <button onClick={() => setCity("current")} className="current-city">
+      <button
+        onClick={() => setCity("current")}
+        className={`current-city ${selectedCity === "current" ? "active" : ""}`}
+      >
         현재 위치
       </button>
       {/* map 함수로 불러오기 */}
 
       <div className="button-wrap">
         {cities.map((city) => (
-          <button key={city} onClick={() => setCity(city)}>
+          <button
+            key={city}
+            onClick={() => setCity(city)}
+            className={selectedCity === city ? "active" : ""}
+          >
             {city}
           </button>
         ))}
